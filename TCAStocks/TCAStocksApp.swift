4a7353a9748190998fe1f4c120a521e80d9a8336
Alex Solonevich <alexander.solonevich@quanti.cz>
2022-11-13 19:08:17 +0100

    Initial Commit

---
+++ TCAStocks/TCAStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAStocksApp.swift
+//  TCAStocks
+//
+//  Created by Alex Solonevich on 13.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TCAStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
